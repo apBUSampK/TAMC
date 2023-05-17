322da2c8d233b16c3ed8d02ede937f0bcab542fe
apBUSampK <sdsnm737@gmail.com>
2023-05-17 23:00:44 +0300

    Removed measuring

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,26 +5,23 @@ import mcinterference.*
 import space.kscience.kmath.complex.Complex
 import space.kscience.kmath.geometry.Euclidean3DSpace
 import space.kscience.kmath.random.RandomGenerator
-import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
-        print(measureTimeMillis {
-                val width = 100
-                val height = 100
-                val context = Integration(720E-9, threads = 4)
-                val pointSource = PointSource(Wave(Euclidean3DSpace.vector(.0, .0, .0), Complex(100)))
-                val slm = SLM(
-                        pointSource,
-                        10.0,
-                        RectangleSampler(.00002, .00002),
-                        { Complex(1) },
-                        RandomGenerator.default(1),
-                        context
-                )
-                val screen = Screen(slm, Euclidean3DSpace.vector(-.5, -.5, 5), .01, .01, width, height)
-                val data = runBlocking(Dispatchers.Default) {
-                        screen.draw(100000, context)
-                }
-                createPng(data, "test")
-        })
+        val width = 100
+        val height = 100
+        val context = Integration(720E-9, threads = 4)
+        val pointSource = PointSource(Wave(Euclidean3DSpace.vector(.0, .0, .0), Complex(100)))
+        val slm = SLM(
+                pointSource,
+                10.0,
+                RectangleSampler(.00002, .00002),
+                { Complex(1) },
+                RandomGenerator.default(1),
+                context
+        )
+        val screen = Screen(slm, Euclidean3DSpace.vector(-.5, -.5, 5), .01, .01, width, height)
+        val data = runBlocking(Dispatchers.Default) {
+                screen.draw(100000, context)
+        }
+        createPng(data, "test")
 }
